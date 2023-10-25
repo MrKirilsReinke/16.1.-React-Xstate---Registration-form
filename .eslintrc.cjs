f833ac2ace2953f2f99a4ca542c69e402865d6b2
root <root@LAPTOP-F9381GQ3>
2023-10-25 15:21:46 +0300

    First commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    "indent": "off",
+    "@typescript-eslint/indent": ['error', 2],
+    'linebreak-style': 'off', // Disable linebreak style rule
+    'quotes': ['error', 'single'], // Enforce single quotes
+    'semi': ['error', 'always'], // Enforce semicolons
+    'no-multi-spaces': 'error', // Disallow multiple spaces
+    'no-multiple-empty-lines': 'error', // Disallow multiple empty lines
+    'comma-dangle': ['error', 'never'], // Enforce trailing commas
+  },
+}
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-1xl font-bold underline">
+        Hello world! lgslkgs
+      </h1>
+    </>
+  );
+}
+
+export default App;
