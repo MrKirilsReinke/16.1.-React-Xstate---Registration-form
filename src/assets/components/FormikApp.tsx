574173313ce149b5e5e1a836dc8550279f19e060
MrKirilsReinke <reinke.kirils@gmail.com>
2023-11-06 19:02:31 +0200

    Refactoring

--- src/assets/components/FormikApp.tsx
+++ src/assets/components/FormikApp.tsx
@@ -302,7 +302,7 @@ function FormikApp() {
                                       Back
                                     </FormikButton>
                                     <FormikButton
-                                      disabled={!formik.isValid || !formik.touched.userName}
+                                      // disabled={!formik.isValid || !formik.touched.userName}
                                       onClick={() => {
                                         send({
                                           type: 'SUBMIT_PERSONAL_INFO'
@@ -421,7 +421,7 @@ function FormikApp() {
                                       Back
                                     </FormikButton>
                                     <FormikButton
-                                      disabled={!formik.isValid || !formik.touched.phoneNumber}
+                                      // disabled={!formik.isValid || !formik.touched.phoneNumber}
                                       onClick={() => {
                                         send({
                                           type: 'SUBMIT_CONTACT_INFO',
@@ -448,58 +448,117 @@ function FormikApp() {
                           Please, check your competed form one more time and press "Confirm" button
                           to complete your registration.
                         </p>
-                        <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                          <div className="sm:col-span-2">
-                            <p className="block text-sm font-medium leading-6 text-gray-300">
-                              First name:
-                            </p>
-                            <p className="mt-1 text-gray-300">{currentState.context.userName}</p>
-                          </div>
-                          <div className="sm:col-span-2">
-                            <p className="block text-sm font-medium leading-6 text-gray-300">
-                              Last name:
-                            </p>
-                            <p className="mt-1 text-gray-300">{currentState.context.lastName}</p>
-                          </div>
-                          <div className="sm:col-span-2">
-                            <p className="block text-sm font-medium leading-6 text-gray-300">
-                              Date of birth:
-                            </p>
-                            <p className="mt-1 text-gray-300">{currentState.context.dateOfBirth}</p>
-                          </div>
-                          <div className="sm:col-span-2">
-                            <p className="block text-sm font-medium leading-6 text-gray-300">
-                              Phone number:
-                            </p>
-                            <p className="mt-1 text-gray-300">{currentState.context.phoneNumber}</p>
-                          </div>
-                          <div className="sm:col-span-2">
-                            <p className="block text-sm font-medium leading-6 text-gray-300">
-                              Street address:
-                            </p>
-                            <p className="mt-1 text-gray-300">
-                              {currentState.context.streetAddress}
+                      </div>
+
+                      <div className="mt-5 space-y-10 divide-y divide-gray-900/10 bg-white p-8 shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl">
+                        <div className="grid grid-cols-1 gap-x-8 gap-y-8 md:grid-cols-3 mt-10">
+                          <div className="px-4 sm:px-0">
+                            <h2 className="text-start text-lg font-semibold leading-7 text-black">
+                              Personal Inforamtion
+                            </h2>
+                            <p className="text-start mt-1 text-sm leading-6 text-black">
+                              Please, check your personal information to ensure everything is correct.
                             </p>
                           </div>
-                          <div className="sm:col-span-2">
-                            <p className="block text-sm font-medium leading-6 text-gray-300">
-                              Country:
-                            </p>
-                            <p className="mt-1 text-gray-300">{currentState.context.country}</p>
-                          </div>
-                          <div className="sm:col-span-2">
-                            <p className="block text-sm font-medium leading-6 text-gray-300">
-                              City:
-                            </p>
-                            <p className="mt-1 text-gray-300">{currentState.context.city}</p>
-                          </div>
-                          <div className="sm:col-span-2">
-                            <p className="block text-sm font-medium leading-6 text-gray-300">
-                              Postal code:
-                            </p>
-                            <p className="mt-1 text-gray-300">{currentState.context.zip}</p>
+                          <div className="bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl md:col-span-2">
+                            <div className="px-4 py-6 sm:p-8">
+                              <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                                <div className="sm:col-span-3">
+                                  <p className="block text-sm font-medium leading-6 text-black">
+                                    First name:
+                                  </p>
+                                  <p className="mt-1 text-black">{currentState.context.userName}</p>
+                                </div>
+                                <div className="sm:col-span-3">
+                                  <p className="block text-sm font-medium leading-6 text-black">
+                                    Last name:
+                                  </p>
+                                  <p className="mt-1 text-black">{currentState.context.lastName}</p>
+                                </div>
+                                <div className="sm:col-span-3">
+                                  <p className="block text-sm font-medium leading-6 text-black">
+                                    Date of birth:
+                                  </p>
+                                  <p className="mt-1 text-black">
+                                    {currentState.context.dateOfBirth}
+                                  </p>
+                                </div>
+                                <div className="sm:col-span-3 sm:col-start-1">
+                                  <p className="block text-sm font-medium leading-6 text-black">
+                                    Username:
+                                  </p>
+                                  <p className="mt-1 text-black">
+                                    {currentState.context.userName}
+                                  </p>
+                                </div>
+                                <div className="sm:col-span-3 sm:col-start-1">
+                                  <p className="block text-sm font-medium leading-6 text-black">
+                                    E-mail:
+                                  </p>
+                                  <p className="mt-1 text-black">
+                                    {currentState.context.eMail}
+                                  </p>
+                                </div>
+                              </div>
+                            </div>
                           </div>
                         </div>
+
+                        <div className="grid grid-cols-1 gap-x-8 gap-y-8 md:grid-cols-3 py-10">
+                          <div className="px-4 sm:px-0">
+                            <h2 className="text-start text-lg font-semibold leading-7 text-black">
+                              Contact Inforamtion
+                            </h2>
+                            <p className="text-start mt-1 text-sm leading-6 text-black">
+                            Please, check your contact information to ensure everything is correct.
+                            </p>
+                          </div>
+                          <div className="bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl md:col-span-2">
+                            <div className="px-4 py-6 sm:p-8">
+                              <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                                <div className="sm:col-span-3">
+                                  <p className="block text-sm font-medium leading-6 text-black">
+                                    Phone number:
+                                  </p>
+                                  <p className="mt-1 text-black">{currentState.context.phoneNumber}</p>
+                                </div>
+                                <div className="sm:col-span-3">
+                                  <p className="block text-sm font-medium leading-6 text-black">
+                                    Street address:
+                                  </p>
+                                  <p className="mt-1 text-black">{currentState.context.streetAddress}</p>
+                                </div>
+                                <div className="sm:col-span-3">
+                                  <p className="block text-sm font-medium leading-6 text-black">
+                                    City:
+                                  </p>
+                                  <p className="mt-1 text-black">
+                                    {currentState.context.dateOfBirth}
+                                  </p>
+                                </div>
+                                <div className="sm:col-span-3">
+                                  <p className="block text-sm font-medium leading-6 text-black">
+                                    State:
+                                  </p>
+                                  <p className="mt-1 text-black">
+                                    {currentState.context.userName}
+                                  </p>
+                                </div>
+                                <div className="sm:col-span-3 sm:col-start-1">
+                                  <p className="block text-sm font-medium leading-6 text-black">
+                                    Country:
+                                  </p>
+                                  <p className="mt-1 text-black">
+                                    {currentState.context.userName}
+                                  </p>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+
+                      <div className="flex flex-col justify-center items-center gap-8">
                         <div className="grid grid-cols-2 gap-10 w-[300px] m-auto mt-10">
                           <FormikButton
                             onClick={() => {
